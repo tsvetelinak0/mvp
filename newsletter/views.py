405326d0934a9d54a5f9c378ed72dbfe34ef7a4e
Tsvetelina Koleva <tsekoleva@gmail.com>
2019-09-18 21:37:42 +0100

    django18 first commit

---
+++ newsletter/views.py
@@ -0,0 +1,74 @@
+from django.conf import settings
+from django.core.mail import send_mail
+from django.shortcuts import render
+
+from .forms import ContactForm, SignUpForm
+# Create your views here.
+def home(request):
+    title = 'Welcome'
+    form = SignUpForm(request.POST or None)
+    context = {
+        "title": title,
+        "form": form
+    }
+    # if request.user.is_authenticated():
+    #     title = "My Title %s" %(request.user)
+    # if request.method == "POST":
+    #     print request.POST
+# add a form
+
+    if form.is_valid():
+        # form.save()
+        # print request.POST['email']  #not recommended, overriding saving the form and cleaned_data; not being validated at all
+        instance = form.save(commit=False)
+        full_name = form.cleaned_data.get("full_name")  #going through the validation and getting data
+        if not full_name:   #if not anything  filled in that field
+            full_name = "New full name"
+        instance.full_name = full_name
+        #if not instance.full_name:
+        #  instance.full_name = "Koleva"
+        instance.save()
+        context = {
+            "title": "Thank you!"
+        }
+    return render(request, "home.html", {})
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        # for key, value in form.cleaned_data.iteritems():
+        #     print key, value
+        # for key in form.cleaned_data:
+        #     print key
+        #     print form.cleaned_data.get(key)
+        form_email = form.cleaned_data.get("email")
+        form_message = form.cleaned_data.get("message")
+        form_full_name = form.cleaned_data.get("full_name")
+        # print email, message, full_name
+        subject = "Site contact form"
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [from_email, "myotheremail@gmail.com"]
+        contact_message = "%s: %s via %s" %(
+                form_full_name,
+                form_message,
+                form_email)
+
+        send_mail(subject,
+                contact_message,
+                from_email,
+                to_email,
+                fail_silently=False)
+
+
+    context = {
+        "form": form,
+    }
+    return render(request, "forms.html", context)
+
+
+
+
+
+
+
+    return render(request, "home.html", context)
