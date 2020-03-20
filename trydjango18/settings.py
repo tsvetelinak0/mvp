b4d2a45b37d0d2c7cdef1f9b9918942a954460d2
Tsvetelina Koleva <tsekoleva@gmail.com>
2020-03-20 09:25:24 +0000

    authentication navbar

--- trydjango18/settings.py
+++ trydjango18/settings.py
@@ -138,6 +138,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap3"
 ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, use a different value.
 REGISTRATION_AUTO_LOGIN = True # Automatically log the user in.
 SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
 
 
 
